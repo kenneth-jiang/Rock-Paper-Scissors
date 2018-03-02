864aa659d593b643a39c149ed462e9054acaa3c0
Kenneth <26774728+kenneth-jiang@users.noreply.github.com>
2018-03-02 05:39:27 -0500

    Refactor.

--- RockPaperScissorsAgainstComputer.py
+++ RockPaperScissorsAgainstComputer.py
@@ -11,35 +11,24 @@ elif random == 2:
 print("Welcome to Rock, Paper, Scissors 2.0! \n Make your choice!")
 user_input = input().lower()
 
+print(f"Computer has picked {choice}!")
+
 if user_input == choice:
-	print(f"Computer has picked {choice}!")
 	print("It's a tie, try again!")
 elif user_input == "rock":
 	if choice == "paper":
-		print(f"Computer has picked {choice}!")
 		print("Computer wins!")
 	elif choice == "scissors":
-		print(f"Computer has picked {choice}!")
 		print("Player wins!")
-	else:
-		print("An error occured, please try again!")
 elif user_input == "paper":
 	if choice == "scissors":
-		print(f"Computer has picked {choice}!")
 		print("Computer wins!")
 	elif choice == "rock":
-		print(f"Computer has picked {choice}!")
 		print("Player wins!")
-	else:
-		print("An error occured, please try again!")
 elif user_input == "scissors":
 	if choice == "rock":
-		print(f"Computer has picked {choice}!")
 		print("Computer wins!")
 	elif choice == "paper":
-		print(f"Computer has picked {choice}!")
 		print("Player wins!")
-	else:
-		print("An error occured, please try again!")
 else:
 	print("An error occured, please try again!")
