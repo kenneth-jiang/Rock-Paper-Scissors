d5eedb1b455b2522c67743edd3740c8f8d0c4a8a
Kenneth <26774728+kenneth-jiang@users.noreply.github.com>
2018-03-01 23:02:21 -0500

    Refactor

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,6 +1,9 @@
 print("Rock, Paper, Scissors, shoot!")
-user1 = input()
-user2 = input()
+
+user1 = input("Player 1, make your move: ")
+print("*** NO CHEATING *** \n\n" * 20)
+user2 = input("Player 2, make your move: ")
+
 if user1 == user2:
 	print("Both tie!")
 elif user1 == "rock":
@@ -9,20 +12,20 @@ elif user1 == "rock":
 	elif user2 == "scissors":
 		print("User 1 wins!")
 	else:
-		print("Please try again!")		
+		print("An error occured, please try again!")	
 elif user1 == "paper":
 	if user2 == "rock":
 		print("User 1 wins!")
 	elif user2 == "scissors":
 		print("User 2 wins!")
 	else:
-		print("Please try again!")		
+		print("An error occured, please try again!")		
 elif user1 == "scissors":
 	if user2 == "rock":
 		print("User 2 wins!")
 	elif user2 == "paper":
 		print("User 1 wins!")
 	else:
-		print("Please try again!")
+		print("An error occured, please try again!")
 else:
-	print("Please try again!")
+	print("An error occured, please try again!")
